60159f0ea36921fa68711c486a16ff33c09e9e16
hikem1 <hikem1@hotmail.fr>
2024-10-03 14:12:03 +0200

    ADD graphTab component

---
+++ src/app/graph-tab/graph-tab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-tab',
+  standalone: true,
+  imports: [],
+  templateUrl: './graph-tab.component.html',
+  styleUrl: './graph-tab.component.scss'
+})
+export class GraphTabComponent {
+
+}
