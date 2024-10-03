7fa4165c6300016342e6186adb04448716b92ada
hikem1 <hikem1@hotmail.fr>
2024-10-03 14:33:17 +0200

    ADD graphTabNav component

--- src/app/graph-tabs/graph-tabs.component.ts
+++ src/app/graph-tabs/graph-tabs.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { GraphTabComponent } from '../graph-tab/graph-tab.component';
+import { GraphTabNavComponent } from '../graph-tab-nav/graph-tab-nav.component';
 
 @Component({
   selector: 'app-graph-tabs',
   standalone: true,
   imports: [
+    GraphTabNavComponent,
     GraphTabComponent
   ],
   templateUrl: './graph-tabs.component.html',
