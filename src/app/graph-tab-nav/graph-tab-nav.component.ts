c57b1ce11b2c1502b6d62e16600a9f4e095e46e5
hikem1 <hikem1@hotmail.fr>
2024-10-04 10:31:13 +0200

    UPDATE graphTabNav with service, graphTab, Ngclass

--- src/app/graph-tab-nav/graph-tab-nav.component.ts
+++ src/app/graph-tab-nav/graph-tab-nav.component.ts
@@ -1,14 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { Instrument } from '../models/instrument';
+import { GraphTabComponent } from '../graph-tab/graph-tab.component';
+import { NgClass } from '@angular/common';
+import { InstrumentService } from '../services/instrument.service';
 
 @Component({
   selector: 'app-graph-tab-nav',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './graph-tab-nav.component.html',
   styleUrl: './graph-tab-nav.component.scss'
 })
 
 export class GraphTabNavComponent {
   @Input() instrument!: Instrument
+  graphTabComponent!: GraphTabComponent;
+  
+  constructor(private instrumentservice: InstrumentService){
+
+  }
+  onActiveTab(instrument: Instrument): void{
+    this.instrumentservice.setActiveInstrument(instrument);
+  }
 }
+
