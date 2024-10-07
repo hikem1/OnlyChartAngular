7778a5d4a0392b89c4fd086660e525a65ae382e4
hikem1 <hikem1@hotmail.fr>
2024-10-07 09:49:35 +0200

    UPDATE instrument service with favorite service

--- src/app/services/instrument.service.ts
+++ src/app/services/instrument.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from '@angular/core';
-import { Instrument } from '../models/instrument';
 import instruments from '../../../public/assets/fixtures/instruments.json';
+import { Instrument } from '../models/instrument';
+import { FavoriteInstrumentsService } from './favorite-instruments.service';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class InstrumentService {
-  private instruments: Instrument[] = []
-
-  constructor(){
+  private instruments: Instrument[] = [];
+  
+  constructor(private favoriteInstrumentsService: FavoriteInstrumentsService){
     instruments.forEach(instrument => 
-      this.instruments.push(new Instrument(instrument))
+      this.addInstrument(instrument)
     )
+  }
+  addInstrument(instrument: Instrument): void{
+    const instrumentToAdd: Instrument = new Instrument(instrument);
+    this.setFavoriteInstrument(instrumentToAdd);
+    this.instruments.push(instrumentToAdd);
+  }
+  removeInstrument(instrument: Instrument): void{
+    this.instruments = this.instruments.filter(instr => instr.id !== instrument.id);
   }
   getInstruments(): Instrument[]{
     return [...this.instruments]
+  }
+  setFavoriteInstrument(instrument: Instrument){
+    if(this.favoriteInstrumentsService.isFavorite(instrument)){
+      instrument.favorite = true;
+    }  
   }
   setActiveInstrument(activeInstrument: Instrument): void{
     this.instruments.forEach(instrument => {
