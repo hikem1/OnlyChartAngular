53fbb346f9bae152b4c8caf34541f58a970f163c
hikem1 <hikem1@hotmail.fr>
2024-10-15 17:16:47 +0200

    ADD form to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,9 @@
 import { NgClass } from '@angular/common';
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, Injectable, inject, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { SearchInstrumentsService } from '../services/search-instruments.service';
 
 @Component({
   selector: 'app-header',
@@ -8,16 +11,29 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
   imports: [
     RouterLink,
     RouterLinkActive,
-    NgClass
+    NgClass,
+    ReactiveFormsModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
+@Injectable({
+  providedIn: 'root',
+})
+
 export class HeaderComponent{
   @ViewChild('navbarToggler') navbarToggler!: ElementRef;
   @ViewChild('navbarContent') navbarContent!: ElementRef;
+  private http = inject(HttpClient)
+  form: FormGroup = new FormGroup({
+    search: new FormControl('')
+  });
 
-  constructor(private router: Router){
+  constructor(
+    private router: Router,
+    private searchInstrumentsService: SearchInstrumentsService
+  ){
   }
   onLogoClick(): void{
     this.router.navigateByUrl("");
@@ -27,5 +43,11 @@ export class HeaderComponent{
     if(this.navbarContent.nativeElement.classList.contains('show')){
       this.navbarToggler.nativeElement.click();
     }
+  }
+  onSubmit(){
+    const keyword: string = this.form.value.search
+    this.router.navigate(["search"], { queryParams: {keyword: keyword} });
+    this.onNavClick();
+    
   }
 }
