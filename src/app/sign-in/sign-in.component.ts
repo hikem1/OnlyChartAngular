b5f0e54bf9a35042cdeb62516c4321b582f61d14
hikem1 <hikem1@hotmail.fr>
2024-10-15 17:18:21 +0200

    UPDAT form credential

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, Injectable } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
-export class SignInComponent {
 
+@Injectable({
+  providedIn: 'root',
+})
+
+export class SignInComponent {
+  private http = inject(HttpClient)
+
+  form: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  constructor(){
+  }
+  onSubmit(){
+    this.http.post('http://localhost:8000/app/api', this.form.value).subscribe(data => {
+      console.log(data);
+    });
+  }
 }
