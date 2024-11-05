467e27a931e58b9b90c81d04c5933d38541645d5
hikem1 <hikem1@hotmail.fr>
2024-11-05 12:08:25 +0100

    UPDATE apiUrl with environment

--- src/app/services/search-instruments.service.ts
+++ src/app/services/search-instruments.service.ts
@@ -2,19 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Instrument } from '../models/instrument';
 import { FavoriteInstrumentsService } from './favorite-instruments.service';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchInstrumentsService {
   matchInstruments: Instrument[] = [];
+  apiUrl: string = environment.apiUrl;
 
   constructor(
     private favoriteInstrument: FavoriteInstrumentsService,
     private http: HttpClient
   ) {}
   search(keyword: string){
-    return this.http.get('http://4.233.147.4:3000/search/' + keyword);
+    return this.http.get(`${this.apiUrl}/search/` + keyword);
   }
   pushInstruments(instruments: Instrument[]){
     this.matchInstruments = [];
