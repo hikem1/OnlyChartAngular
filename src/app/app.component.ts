e50a4829b0fcfda8114adce9286a178a88ea03dc
hikem1 <hikem1@hotmail.fr>
2024-11-05 12:06:08 +0100

    UPDATE config loader-intercept add popover loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ErrorPopoverComponent } from './error-popover/error-popover.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    ErrorPopoverComponent,
+    LoaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'OnlyChartAngular';
+  constructor(){
+
+  }
 }
