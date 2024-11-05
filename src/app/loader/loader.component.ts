5e152796a70d146b3154a1ca16d24cb1687d2d43
hikem1 <hikem1@hotmail.fr>
2024-11-05 12:04:46 +0100

    UPDATE loader component

--- src/app/loader/loader.component.ts
+++ src/app/loader/loader.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, timer } from 'rxjs';
+import { LoaderService } from '../services/loader.service';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-loader',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    AsyncPipe
+  ],
   templateUrl: './loader.component.html',
   styleUrl: './loader.component.scss'
 })
-export class LoaderComponent {
+export class LoaderComponent{
+  loading$!: Observable<boolean>;
+
+  constructor(private loaderService: LoaderService) {
+    this.loading$ = this.loaderService.loading$;
+  }
 }
