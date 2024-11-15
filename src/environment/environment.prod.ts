1bf4a30910ba876291a227bd8d90454bb0336ab5
Rouchon Fabien <hikem1@hotmail.fr>
2024-11-15 10:49:11 +0100

    ENV ionos

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://4.233.147.4:3000',
-    apiUrl: 'https://only-chart.hikem.fr/api:3000',
+    apiUrl: 'https://only-chart.hikem.fr:3000',
   };
   
