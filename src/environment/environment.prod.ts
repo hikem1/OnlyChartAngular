a4f4cefd964509b1dca2fa5d67f5a315ea84b5fb
Rouchon Fabien <hikem1@hotmail.fr>
2024-11-15 12:47:00 +0100

    BUILD

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://4.233.147.4:3000',
-    apiUrl: 'http://127.0.0.1:3000'
+    apiUrl: 'http://87.106.137.135:3000'
   };
   
