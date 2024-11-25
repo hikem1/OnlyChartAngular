25ae73df6ab08a0d30ca56115fc318c339b0b338
Rouchon Fabien <hikem1@hotmail.fr>
2024-11-25 09:50:30 +0100

    UPDATE apiurl ngrok

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://4.233.147.4:3000',
-    apiUrl: 'https://only-chart.hikem.fr/api'
+    // apiUrl: 'https://only-chart.hikem.fr/api'
+    apiUrl: 'https://shining-frankly-urchin.ngrok-free.app/api'
   };
   
